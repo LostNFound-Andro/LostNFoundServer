4167a400ea73cccc9bc44ff0051dc0aab793e451
simsarulhaqv <simsarulhaqv@gmail.com>
2016-04-14 17:18:55 +0530

    Update feed by posting only subscribed items

--- getpost.php
+++ getpost.php
@@ -1,11 +1,12 @@
-
 <?php
-$con = new mysqli("localhost", "UserName", "Password", "maindb");
+$con = new mysqli("localhost", "root", "sel-lfa", "maindb");
 if($con ->connect_error){
     die("error connectiong");
 }
+
+$email = $_POST["email"];
 $con -> query($sql);
-$res = $con -> query("SELECT title, description, cid, email, time, date , location FROM `found_post`");
+$res = $con -> query("SELECT found_post.title as title, found_post.description as post, found_post.cid as cid, found_post.email as email, found_post.time as time, found_post.date as date , found_post.location as location FROM `found_post`, `subscription` WHERE subscription.email='$email' and  subscription.cid = found_post.cid");
 $o = array();
 while($row = $res->fetch_assoc()){
     $o[] = $row;
@@ -13,6 +14,4 @@ while($row = $res->fetch_assoc()){
 }
 $final=array('postlist'=>$o);
 print(json_encode($final));
-
 ?>
-
